94daf7424894388c950ae48f139a234e820ece84
Berke Sayın <sayinberkesayin@gmail.com>
2023-02-13 23:26:10 +0300

    Add reducers, create store and set up redux devtools extension.

--- redux-with-react/src/index.js
+++ redux-with-react/src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+import allReducers from "./reducers/index";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
